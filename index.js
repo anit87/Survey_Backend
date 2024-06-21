903af463fee5fc8a73eac7d58acc59dd0cccc286
anit87 <anit87@bwsteam.com>
2024-06-21 12:42:40 +0530

    fixes

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const connectDb = require("./utils/db")
-const formsRouter = require("./routes/forms/surveyForms")
-const authRouter = require("./routes/auth/auth")
-const usersRouter = require("./routes/users/users")
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 const cookieParser = require('cookie-parser');
+const connectDb = require('./utils/db');
+const formsRouter = require('./routes/forms/surveyForms');
+const authRouter = require('./routes/auth/auth');
+const usersRouter = require('./routes/users/users');
 
-const port = 4000
-connectDb()
-app.use(cors())
+const app = express();
+const port = 4000;
+
+// Read SSL certificate files
+const privateKey = fs.readFileSync('./certs/server.key', 'utf8');
+const certificate = fs.readFileSync('./certs/server.cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+connectDb();
+
+app.use(cors());
 app.use(cookieParser());
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/uploads', express.static('public'))
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/uploads', express.static('public'));
 
-app.use("/auth", authRouter)
-app.use("/forms", formsRouter)
-app.use("/users", usersRouter)
+app.use("/auth", authRouter);
+app.use("/forms", formsRouter);
+app.use("/users", usersRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is Running' });
@@ -28,6 +37,14 @@ app.get('/test-cookie', (req, res) => {
   res.json({ message: 'Cookie set successfully' });
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+// HTTP server
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`)
+// })
+
+// Create HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`App listening on port ${port} over HTTPS`);
+});
