bd57454f70efacaa17d09249c972c87e3e8e13d6
anit87 <anit87@bwsteam.com>
2024-06-26 13:03:23 +0530

    small changes

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const app = express();
 const port = 4000;
 
 // Read SSL certificate files
-const privateKey = fs.readFileSync('./certs/server.key', 'utf8');
-const certificate = fs.readFileSync('./certs/server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+// const privateKey = fs.readFileSync('./certs/server.key', 'utf8');
+// const certificate = fs.readFileSync('./certs/server.cert', 'utf8');
+// const credentials = { key: privateKey, cert: certificate };
 
 connectDb();
 
@@ -38,17 +38,17 @@ app.get('/test-cookie', (req, res) => {
 });
 
 // HTTP server
-// app.listen(port, () => {
-//   console.log(`App listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
 
 // Create HTTPS server
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(port, (error) => {
-  if (error) {
-    console.error('Error starting HTTPS server:', error);
-  } else {
-    console.log(`App listening on port ${port} over HTTPS`);
-  }
-});
+// httpsServer.listen(port, (error) => {
+//   if (error) {
+//     console.error('Error starting HTTPS server:', error);
+//   } else {
+//     console.log(`App listening on port ${port} over HTTPS`);
+//   }
+// });
